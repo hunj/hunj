deef73b032662db8619ad3d2d71320943ffb7664
hunj <hunjaelee@protonmail.com>
2023-07-03 21:16:28 -0400

    fix shit

--- .github/scripts/update_posts.py
+++ .github/scripts/update_posts.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-from github import Github
+from github import Github, Auth
 import os
 import re
 
+TOKEN = os.environ.get('GH_TOKEN')
 URL = "https://hunj.dev"
 HEADER = "### Recent Blog Posts\n"
 REPO = 'hunj/hunj'
@@ -24,7 +25,7 @@ for post in soup.select('article.post'):
 posts.append('\n')
 posts_text = '\n'.join(posts)
 
-shithub = Github("access_token")
+shithub = Github(auth=Auth.Token(TOKEN))
 repo = shithub.get_repo(REPO)
 readme = repo.get_readme()
 
